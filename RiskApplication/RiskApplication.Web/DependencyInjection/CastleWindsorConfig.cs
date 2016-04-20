8e2e394c7969c65a6d5a3858b05bf9872f8696c7
Ehsan <Ehsan.Vali@gmail.com>
2016-04-20 21:17:02 +0430

    Add Castle Windsor DI container to Web Application with Nuget

---
+++ RiskApplication/RiskApplication.Web/DependencyInjection/CastleWindsorConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using Castle.Windsor.Mvc;
+using RiskApplication.Web.DependencyInjection;
+using WebActivatorEx;
+
+[assembly: PreApplicationStartMethod(typeof(CastleWindsorConfig), "Start")]
+[assembly: ApplicationShutdownMethod(typeof(CastleWindsorConfig), "Stop")]
+
+namespace RiskApplication.Web.DependencyInjection
+{
+    public static class CastleWindsorConfig
+    {
+        private static IWindsorContainer _container;
+
+        public static void Start()
+        {
+            _container = new WindsorContainer().Install(FromAssembly.This());
+
+            var controllerFactory = new WindsorControllerFactory(_container.Kernel);
+            ControllerBuilder.Current.SetControllerFactory(controllerFactory);
+        }
+
+        /// <summary>
+        ///     Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+            _container?.Dispose();
+        }
+    }
+}
