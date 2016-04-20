39a43b33f57565113120a31fc150206fd7915e7d
Ehsan <Ehsan.Vali@gmail.com>
2016-04-20 21:27:51 +0430

    Create RiskController in Web Application

---
+++ RiskApplication/RiskApplication.Web/Controllers/RiskController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RiskApplication.Web.Controllers
+{
+    public class RiskController : Controller
+    {
+        // GET: Risk
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
