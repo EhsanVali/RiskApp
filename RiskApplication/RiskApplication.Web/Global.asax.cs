fbdcab8b42fcb76517431bb6c3d1488eadb518e0
Ehsan <Ehsan.Vali@gmail.com>
2016-04-20 22:23:57 +0430

    AutoMapper config

--- RiskApplication/RiskApplication.Web/Global.asax.cs
+++ RiskApplication/RiskApplication.Web/Global.asax.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AutoMapper;
+using RiskApplication.Web.Models.Mapper;
 
 namespace RiskApplication.Web
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
@@ -16,6 +15,11 @@ namespace RiskApplication.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //TODO: Move to DI
+            Mapper.Initialize(a => a.AddProfile<RiskProfile>());
+            Mapper.Initialize(a => a.AddProfile<CustomerProfile>());
+            Mapper.AssertConfigurationIsValid();
         }
     }
 }
