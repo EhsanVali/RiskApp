931082d7d1ddd205e572122fad03f3df7a982c51
Ehsan <Ehsan.Vali@gmail.com>
2016-04-20 20:14:12 +0430

    Unit Test for Unusual Win Rate

---
+++ RiskApplication/RiskApplication.Domain.Test/BusinessRulesTest/UnusualWinRateBusinessRuleTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using RiskApplication.Domain.BusinessRules;
+using RiskApplication.Domain.Models;
+
+namespace RiskApplication.Domain.Test.BusinessRulesTest
+{
+    [TestFixture]
+    public class UnusualWinRateBusinessRuleTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            _rule = new UnusualWinRateBusinessRule();
+        }
+
+        private UnusualWinRateBusinessRule _rule;
+
+        [TestCase(100, 59, false)]
+        [TestCase(100, 60, true)]
+        [TestCase(100, 61, true)]
+        public void ShouldReturnExpectedResult(int totalBets, int totalWins, bool expected)
+        {
+            var result = _rule.IsValid(new Customer
+            {
+                TotalBets = totalBets,
+                TotalBetsWon = totalWins
+            });
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [Test]
+        public void ShouldReturnFalseWhenNullInput()
+        {
+            Assert.IsFalse(_rule.IsValid(null));
+        }
+    }
+}
