b1973d403bde23ab53103c4e30b26ec296f5285c
Ehsan <Ehsan.Vali@gmail.com>
2016-04-20 19:52:31 +0430

    Business rules 2 : Unusual Stake business rule

---
+++ RiskApplication/RiskApplication.Domain/BusinessRules/UnusualStakeBusinessRule.cs
@@ -0,0 +1,17 @@
+﻿using RiskApplication.Domain.Models;
+
+namespace RiskApplication.Domain.BusinessRules
+{
+    public class UnusualStakeBusinessRule
+    {
+        public bool IsSatisfied(CustomerUnsettledBet customerUnsettledBet)
+        {
+            if (customerUnsettledBet?.UnsettledBet == null || customerUnsettledBet.Customer == null)
+            {
+                return false;
+            }
+
+            return customerUnsettledBet.Customer.AverageStake * 10 <= customerUnsettledBet.UnsettledBet.Stake;
+        }
+    }
+}
