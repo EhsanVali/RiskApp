3e7423c859c83aad53542d9be826519bb64118c1
Ehsan <Ehsan.Vali@gmail.com>
2016-04-20 20:04:09 +0430

    Business rules 4 : High price to win business rule

---
+++ RiskApplication/RiskApplication.Domain/BusinessRules/HighPriseBusinessRule.cs
@@ -0,0 +1,12 @@
+﻿using RiskApplication.Domain.Models;
+
+namespace RiskApplication.Domain.BusinessRules
+{
+    public class HighPriseBusinessRule
+    {
+        public bool IsValid(UnsettledBet unsettledBet)
+        {
+            return unsettledBet != null && unsettledBet.ToWin >= 1000;
+        }
+    }
+}
